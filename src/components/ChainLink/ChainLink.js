40224d7160340cb63e4c2d9c392e8513cee9e592
anovikova <ptica-govorun@inbox.ru>
2020-03-10 17:00:20 +0300

    Запретили подгрузку локальных шрифтов и исправили Ариал дважды

---
+++ src/components/ChainLink/ChainLink.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import cn from "classnames";
+
+class CaptionedImage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default CaptionedImage;
