a9554d7b97fc0071340bd5c39d720b12587e8a77
anovikova <ptica-govorun@inbox.ru>
2020-03-23 00:35:12 +0300

    Убрали в геттеры classNames

--- src/components/ChainLink/ChainLink.js
+++ src/components/ChainLink/ChainLink.js
@@ -18,40 +18,49 @@ class ChainLink extends Component {
         return obj;
     }
 
-    render() {
-        const isImage = this.props.img || this.props.symbol;
-        const isArrows = this.props.arrows && this.props.arrows.length > 0;
-        const isTArrows = this.props.tArrows && this.props.tArrows.length > 0;
-
+    get chainLinkClasses() {
         let chainLinkClasses = {
             'chainLink': true,
             [`${this.props.parentClass}__chainLink`]: this.props.parentClass,
             [`chainLink--${this.props.img}`]: this.props.img
         };
 
-        chainLinkClasses = (this.props.modificators) ? cn(this.getModificators(this.props.modificators, chainLinkClasses)) : cn(chainLinkClasses);
+        chainLinkClasses = (this.props.modificators) ?
+                          cn(this.getModificators(this.props.modificators, chainLinkClasses)) :
+                          cn(chainLinkClasses);
+        return chainLinkClasses;
+    }
 
-        const boxClasses = cn({
+    get boxClasses() {
+        return cn({
             'chainLink__box': true,
             [`flex-${this.props.layout}`]: this.props.layout,
             [`flex-align-${this.props.align}`]: this.props.align
         });
+    }
 
-        const contentClasses = cn({
+    get contentClasses() {
+        return cn({
             'chainLink__content': true,
             [`flex-${this.props.contentLayout}`]: this.props.contentLayout,
             [`flex-align-${this.props.contentAlign}`]: this.props.contentAlign
         });
+    }
+
+    render() {
+        const isImage = this.props.img || this.props.symbol;
+        const isArrows = this.props.arrows && this.props.arrows.length;
+        const isTArrows = this.props.tArrows && this.props.tArrows.length;
 
         return (
-            <article className={chainLinkClasses}>
-                <div className={boxClasses}>
+            <article className={this.chainLinkClasses}>
+                <div className={this.boxClasses}>
                     {isImage && <div className='chainLink__image'>
                         {this.props.symbol && <span className='chainLink__symbol'>{this.props.symbol}</span>}
                         {isArrows && this.renderArrows(this.props.arrows)}
                     </div>}
 
-                    <div className={contentClasses}>
+                    <div className={this.contentClasses}>
                         {this.props.index && <div className='chainLink__index'>{this.props.index}</div>}
                         {this.props.text && <div className='chainLink__text'>{this.props.text}</div>}
                         {this.props.innerText && <div dangerouslySetInnerHTML={{__html: this.props.innerText}} className='chainLink__text'/>}
