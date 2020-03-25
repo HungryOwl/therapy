1313c28875ded28fb9489307638126145355d782
anovikova <ptica-govorun@inbox.ru>
2020-03-25 17:38:23 +0300

    слайд Therapy - Зачем класс, когда можно функцию?

--- src/components/Slide/Therapy.js
+++ src/components/Slide/Therapy.js
@@ -1,13 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Therapy extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <h2 className='slide__title'>Основа терапии — патогенез СД2</h2>
-    }
-}
+const Therapy = () => {
+    return <h2 className='slide__title'>Основа терапии — патогенез СД2</h2>
+};
 
 export default Therapy;
